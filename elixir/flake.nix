0bb60c08cc8c27e26418b599fb3dffff4b071070
Joseph Lozano <me@lozanojoseph.com>
2024-03-26 20:33:51 -0400

    add elixir and node flakes

---
+++ elixir/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based Elixir development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = (with pkgs; [ elixir ]) ++
+            # Linux only
+            pkgs.lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [ gigalixir inotify-tools libnotify ]) ++
+            # macOS only
+            pkgs.lib.optionals (pkgs.stdenv.isDarwin) (with pkgs; [ terminal-notifier ]) ++
+            (with pkgs.darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+        };
+      });
+    };
+}
