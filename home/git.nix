f61792c1f22913046d884c341a62773b29e8e135
Joseph Lozano <me@lozanojoseph.com>
2025-01-06 23:23:32 -0500

    add git hist alias

--- home/git.nix
+++ home/git.nix
@@ -42,6 +42,9 @@ _: {
       commit = {
         gpgsign = true;
       };
+      alias = {
+        hist = "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short";
+      };
     };
   };
 }
